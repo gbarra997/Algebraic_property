9b8162934e4c5420c0cdfe887822cd53873485b1
gbarra997 <148078398+gbarra997@users.noreply.github.com>
2024-01-11 00:43:56 +0100

    Test_performance.py

--- Test_performance.py
+++ Test_performance.py
@@ -7,35 +7,37 @@ import subprocess
 import multiprocessing
 import matplotlib.pyplot as plt
 import pandas as pd
+
 parser = argparse.ArgumentParser(description="MATRIX EQUALITY TEST: Test the differences of speed in the alogritmhs")
 
 parser.add_argument("-sv", "--save_file", type=functions.check_save_file,
                     default=os.getcwd(),
                     help="Directory path to save the results. A picture for each modality and a dataset will be saved in the current working directory or in a given one")
 
-v = "n"
+
+
+
 args = parser.parse_args()
 save_directory = args.save_file  # Assuming args.save_file is the directory path
 
 if __name__ == "__main__":
-    sizes = [200,400,800,1000,1200,1600,2000,2500,3000]
-
+    v = "n"
+    sizes = [200,400,800,1600,2400,2500,2800]
+    size_seq = [10,50,100,150,200,250,300]
     times_for_seq = []
     times_for_num = []
-   
-
     times_for_multi = {_ : [] for _ in range(1, multiprocessing.cpu_count())}  # Dictionary to store times for different number of processes
     k = 2
     #RUN SEQUENTIAL
 
     print("Sequential")
-    for elem in range(2,300,25):
+    for elem in size_seq:
         print(f'Testing with {elem} size')
         start_time1 = time.time() 
         for _ in range(1,11):
             functions.fun_for_sequential(elem,k,v)
-        end_time1 = time.time()
-        times_for_seq.append(end_time1 - start_time1 )
+            end_time1 = time.time()
+            times_for_seq.append(end_time1 - start_time1 )
 
 
     #RUN NUMPY MULTI THREAD
@@ -43,21 +45,20 @@ if __name__ == "__main__":
     for elem in sizes:
         print(f'Testing with {elem} size')
         times_for_num.append(functions.whole_numpy(elem,k,v))
-    #RUN NUMPY ONE THREAD
 
+    #RUN NUMPY ONE THREAD
     print("Numpy with only 1 thread")
     python_executable = sys.executable
     processo = subprocess.Popen([python_executable, 'subprocessnumpy.py'], stdout=subprocess.PIPE, text=True)
     output, _ = processo.communicate()
     output = output.strip("[]\n").split(",")
     times_for_num_1 = [float(elem) for elem in output]
-# Convert the extracted numbers to float and recreate the list
-
 
 
     #RUN MULTIPROCESS
     print("Multiprocess")
     for elem in sizes:
+        print(f'Testing with {elem} size')
         for processi in range(1, multiprocessing.cpu_count()):
             times = functions.multi_process(elem, k, processi, v)
             times_for_multi[processi].append(times)
@@ -68,7 +69,7 @@ if __name__ == "__main__":
     # Plot and save for Sequential
 
     plt.figure(figsize=(10, 6))
-    plt.plot(times_for_seq, label="Sequential", marker='o')
+    plt.plot(size_seq,times_for_seq, label="Sequential", marker='o')
     plt.ticklabel_format(style='plain')    # to prevent scientific notation.
     plt.xlabel('Sizes')
     plt.ylabel('Time')
@@ -111,6 +112,7 @@ if __name__ == "__main__":
     data = {
         'Sequential': times_for_seq,
         'Numpy': times_for_num,
+        'Numpy_1': times_for_num_1
     }
     for processi, times in times_for_multi.items():
         data[f'Multiprocessing_{processi}'] = times
